81bd5e190752e80a9dc823ebabbbd6e683d5a389
Ruben Silva <paulo.ras98@gmail.com>
2024-07-04 17:18:25 +0100

    Tutorial/PlanVisit: Completed step 1 and generated distance matrix.

--- src/Tutorial/PlanVisit/Task.cpp
+++ src/Tutorial/PlanVisit/Task.cpp
@@ -51,10 +51,21 @@ namespace Tutorial
     struct Task: public DUNE::Tasks::Task
     {
 
-      // Declare an Arguments Variable
+      //! Declare an Arguments Variable.
       Arguments m_args;  
-      // Operation mode
+      //! Operation mode.
       int op_mode; 
+      //! Position variables of our vehicle.
+      //! Latitude.
+      fp64_t s_lat;
+      //! Longitude.
+      fp64_t s_lon;
+      //! Vector of points to visit 
+      std::vector<double> p_visit; 
+      //! Flag to check if plan has been generated
+      bool is_plan_gen = false;
+      //! Distance matrix 
+      std::vector<std::vector<double>> m_distances; 
 
       //! Constructor.
       //! @param[in] name task name.
@@ -67,13 +78,15 @@ namespace Tutorial
                     Tasks::Parameter::VISIBILITY_USER, true);
 
         // Declare points to visit parameter and get values from .ini file
-                // Declare parameter of message limit
-        param("Points to Visit ", m_args.p_visit)
+        param("Points to Visit", m_args.p_visit)
         .defaultValue("")
-        .description("Points that should be visited by lauv.");
+        .description("Points that should be visited by lauv.")
+        .visibility(Tasks::Parameter::VISIBILITY_USER)
+        .scope(Tasks::Parameter::SCOPE_GLOBAL);
 
         // Bind to the check if vehicle is in service
         bind<IMC::VehicleState>(this);
+        bind<IMC::EstimatedState>(this); 
         
       }
 
@@ -84,11 +97,59 @@ namespace Tutorial
         op_mode = msg->op_mode;
 
       }
+
+      // Keep track of the position of the vehicle
+      void 
+      consume(const IMC::EstimatedState* msg){
+
+        // Convert Position WGS84 format Coordinates 
+        Coordinates::toWGS84(*msg, s_lat, s_lon);
+      }
+
+      bool validateCoor(std::vector<double>& points)
+      {
+        if (points.size() % 2 == 0)
+        {
+          return true; 
+        } 
+        else
+        {
+          return false; 
+        }
+      }
       
-      //! Update internal state with new parameter values.
+      //! In this case new parameters will mean new points to visit! 
       void
       onUpdateParameters(void)
       {
+
+        //! Check if points to visit parameters were changed. 
+        if(paramChanged(m_args.p_visit) )
+        {
+          //! Check if number of points is even,
+          //! If not deactivate the task
+          if(!validateCoor(m_args.p_visit))
+          {
+            throw std::runtime_error("Number of points is not correct.");
+          }
+
+          //! Save the points in a variable
+          for(auto& value : m_args.p_visit)
+          {
+            p_visit.push_back(Angles::radians(value));
+          }
+
+          spew("New, valid set of points inserted! New plan should be generated"); 
+          is_plan_gen = false;
+        }
+      }
+
+      void printVectors(std::vector<double>& vec)
+      {
+        for (auto& value : vec)
+        {
+          spew("Value is: %f", value); 
+        }
       }
 
       //! Reserve entity identifiers.
@@ -125,13 +186,44 @@ namespace Tutorial
       void
       onMain(void)
       {
+
         while (!stopping())
         {
           waitForMessages(1.0);
+
+          // Check if our task is Active and the path has been generated
+          // If it isnt start generating
+          if( (this->isActive()) && (!is_plan_gen) && (op_mode == IMC::VehicleState::VS_SERVICE))
+          {
+
+            spew("Current pos (%f, %f)", s_lat, s_lon);
+
+            // Add the current position as a point to visit in the beginning of the vector
+            p_visit.push_back(s_lat);
+            std::swap(p_visit.front(), p_visit.back());
+            p_visit.push_back(s_lon); 
+            std::swap(p_visit[1], p_visit.back());
+
+            //! I need to calculate the distances between all the points
+            for(unsigned int j = 0; j < p_visit.size();j+=2)
+            {
+              std::vector<double> dist_row;
+              for(unsigned int i = 0; i < p_visit.size();i+=2)
+              {
+                dist_row.push_back(WGS84::distance(p_visit[j], p_visit[j + 1], 0,
+                                            p_visit[i], p_visit[i + 1], 0));
+              } 
+              inf("New distance row: ");
+              printVectors(dist_row);
+              m_distances.push_back(dist_row); 
+
+            }
+            is_plan_gen = true; 
+          }
         }
       }
     };
-  }
+   }
 }
 
 DUNE_TASK
