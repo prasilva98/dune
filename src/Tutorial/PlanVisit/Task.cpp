8bab7fc79d26103721c13cc025f13e102e84af99
Ruben Silva <paulo.ras98@gmail.com>
2024-07-08 14:42:04 +0100

    Tutorial/PlanVisit: Plan is now constructed and sent for control. Exercise is done

--- src/Tutorial/PlanVisit/Task.cpp
+++ src/Tutorial/PlanVisit/Task.cpp
@@ -56,6 +56,8 @@ namespace Tutorial
       Arguments m_args;  
       //! Operation mode.
       int op_mode; 
+      //! Plan Mode; 
+      int p_mode;
       //! Position variables of our vehicle.
       //! Latitude.
       fp64_t s_lat;
@@ -69,12 +71,17 @@ namespace Tutorial
       std::vector<std::vector<double>> m_distances; 
       //! Full Path 
       std::vector<int> m_path; 
+      // Full plan specification 
+      IMC::PlanSpecification p_spec;
+      // Flag to check if plan has been sent 
+      bool plan_sent = false; 
 
       //! Constructor.
       //! @param[in] name task name.
       //! @param[in] ctx context.
       Task(const std::string& name, Tasks::Context& ctx):
-        DUNE::Tasks::Task(name, ctx)
+        DUNE::Tasks::Task(name, ctx),
+        op_mode(-1)
       {
         // Make the Active parameter global and visible to users (in neptus)
         paramActive(Tasks::Parameter::SCOPE_GLOBAL,
@@ -90,23 +97,35 @@ namespace Tutorial
         // Bind to the check if vehicle is in service
         bind<IMC::VehicleState>(this);
         bind<IMC::EstimatedState>(this); 
+        bind<IMC::PlanControlState>(this);
         
       }
 
       // Keep track of the operation mode of the vehicle.
       void 
-      consume(const IMC::VehicleState* msg){
-
+      consume(const IMC::VehicleState* msg)
+      {
         op_mode = msg->op_mode;
-
       }
 
       // Keep track of the position of the vehicle.
       void 
-      consume(const IMC::EstimatedState* msg){
-
+      consume(const IMC::EstimatedState* msg)
+      {
         // Convert Position WGS84 format Coordinates 
         Coordinates::toWGS84(*msg, s_lat, s_lon);
+      }
+
+      //! Keep track of the the state of the plan 
+      void consume(const IMC::PlanControlState* msg)
+      {
+        p_mode = msg->state; 
+
+        if (msg->last_outcome == PlanControlState::LPO_SUCCESS)
+        {
+          inf("Plan is sucessfully completed");
+        }
+
       }
 
       // Validate coordinates based on if the number of points is even or not.
@@ -145,6 +164,13 @@ namespace Tutorial
 
           spew("New, valid set of points inserted! New plan should be generated"); 
           is_plan_gen = false;
+          plan_sent = false; 
+
+          if(op_mode == IMC::VehicleState::VS_MANEUVER)
+          {
+            IMC::Abort msg;
+            dispatch(msg);
+          }
         }
       }
 
@@ -243,48 +269,109 @@ namespace Tutorial
 
           // Check if our task is Active and the path has been generated
           // If it isnt generate a distance matrix and calculate the best route
-          if( (this->isActive()) && (!is_plan_gen) && (op_mode == IMC::VehicleState::VS_SERVICE))
+
+          if ( (op_mode == IMC::VehicleState::VS_SERVICE) && (this->isActive()) )
           {
 
-            spew("Current pos (%f, %f)", s_lat, s_lon);
-            
-            //! Add current position to the beginning of the list of points to visit
-            addCurrentPosition();
-            calcDistMatrix();            
-            
-            // Fill the path variable based on the number of points 
-            for (unsigned int i = 0; i < m_distances.size(); i++)
+            if(!is_plan_gen)
             {
-              m_path.push_back(i);
-            }
 
-            // First minDistance is the biggest distance that fits inside a double.
-            double minDistance = std::numeric_limits<double>::max();
-
-            // Save the best path on this variable. 
-            std::vector<int> bestPath; 
-            do
-            {
-              int currentDistance = CalculateTotalDistance();
-              if (currentDistance < minDistance) 
+              spew("Current pos (%f, %f)", s_lat, s_lon);
+              
+              //! Add current position to the beginning of the list of points to visit
+              addCurrentPosition();
+              calcDistMatrix();            
+              
+              // Fill the path variable based on the number of points 
+              for (unsigned int i = 0; i < m_distances.size(); i++)
               {
-                minDistance = currentDistance;
-                bestPath = m_path; 
+                m_path.push_back(i);
               }
 
-            }while(std::next_permutation(m_path.begin() + 1, m_path.end()));
+              // First minDistance is the biggest distance that fits inside a double.
+              double minDistance = std::numeric_limits<double>::max();
 
-            std::cout << "Best path with a distance of " << minDistance << " is: " << std::endl;  
+              // Save the best path on this variable. 
+              std::vector<int> bestPath; 
+              do
+              {
+                int currentDistance = CalculateTotalDistance();
+                if (currentDistance < minDistance) 
+                {
+                  minDistance = currentDistance;
+                  bestPath = m_path; 
+                }
 
-            for(const auto& value: bestPath)
-            {
-              std::cout << value << " "; 
+              }while(std::next_permutation(m_path.begin() + 1, m_path.end()));
+
+              //! Creating the various maneuvers for the plans
+              for(unsigned int i = 0; i < bestPath.size(); i++)
+              {
+                double lat = p_visit[bestPath[i]*2]; 
+                double lon = p_visit[bestPath[i]*2 + 1];
+
+                IMC::Goto goto_maneuver; 
+                goto_maneuver.lat = lat; 
+                goto_maneuver.lon = lon; 
+                goto_maneuver.speed = 1; 
+                inf("lat to go: %f, lon to go: %f", goto_maneuver.lat, goto_maneuver.lon);
+                goto_maneuver.speed_units = IMC::SpeedUnits::SUNITS_METERS_PS;
+                goto_maneuver.z = 0; 
+                goto_maneuver.z_units = IMC::Z_DEPTH;  
+
+                // Set Plan Maneuver
+                IMC::PlanManeuver pman;
+                std::stringstream man_name; 
+                man_name << "Goto " << i; 
+                pman.maneuver_id = man_name.str();
+                //! Start filling the actual maneuver plan
+                pman.data.set(goto_maneuver);
+                
+                p_spec.maneuvers.push_back(pman);
+                
+                // The beginning maneuver
+                if (i == 1)
+                {
+                  p_spec.start_man_id = pman.maneuver_id; 
+                }
+                else
+                {
+                // After the initial maneuver ID it is required to create transitions between maneuvers;
+
+                  IMC::PlanTransition ptrans; 
+                  std::stringstream prev_man_name; 
+                  prev_man_name << "Goto " << i - 1; 
+                  ptrans.source_man = prev_man_name.str(); 
+                  ptrans.dest_man = man_name.str();
+                  ptrans.conditions = "ManeuverIsDone";
+                  p_spec.transitions.push_back(ptrans);
+                  
+                }
+              } 
+
+              // Plan is finally generated.
+              is_plan_gen = true;
             }
+            
+            // If the plan has been generated but not sent
+            if ( (!plan_sent) && (is_plan_gen) )
+            {
+              
+              auto m_gen = Math::Random::Factory::create(Math::Random::Factory::c_default);
 
-            std::cout << std::endl; 
+              inf("Plan control is being set");
+              IMC::PlanControl plan_control;
+              plan_control.type = IMC::PlanControl::PC_REQUEST;
+              plan_control.op = IMC::PlanControl::PC_START;
+              plan_control.request_id = m_gen->random() & 0xFFFF;
+              plan_control.plan_id = p_spec.plan_id;
+              plan_control.arg.set(p_spec);
+              plan_control.setDestination(getSystemId());  
+              dispatch(plan_control); 
 
-            is_plan_gen = true;
-
+              plan_sent = true;    
+  
+            }
           }
         }    
       }
